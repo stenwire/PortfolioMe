493c1a463ff4ea8f255e4b63edd80b9ae4a0e887
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-05-22 12:29:22 +0100

    add files

---
+++ backend/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from blog.models import Article
+
+
+class BlogAdminSite(admin.AdminSite):
+    site_header = "Portfolio Administration"
+    site_title = "Portfolio"
+    index_title = "Portfolio Administration"
+    empty_value_display = "- - - -"
+
+
+admin_site = BlogAdminSite(name="Portfolio Admin Site")
